26911ef3fba649732e82c3498d1f54ce616c93db
Ian Heggie <ian@heggie.biz>
2018-03-18 06:37:45 +1100

    first commit

---
+++ lib/health_check_activerecord.rb
@@ -0,0 +1,5 @@
+require "health_check_activerecord/version"
+
+module HealthCheckActiverecord
+  # Your code goes here...
+end
---
+++ lib/health_check_activerecord/version.rb
@@ -0,0 +1,3 @@
+module HealthCheckActiverecord
+  VERSION = "0.1.0"
+end
